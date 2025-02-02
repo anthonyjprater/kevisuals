30cd797905664dc3c4e99ffebcaaf4cc328242d5
ajp1984 <prater.anthony.j@gmail.com>
2025-02-02 18:29:48 +0000

    added loading blur

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,31 @@
+const bg = document.querySelector('.background-img')
+const loadText = document.querySelector('.loading-text')
+
+let load = 0
+
+let int = setInterval(blurring, 30)
+
+function blurring() {
+    load++
+
+    if ( load > 99 ) {
+        clearInterval(int)
+    }
+
+    loadText.innerText = `${load}%`
+    loadText.style.opacity = scale(load, 0, 100, 1, 0)
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+    bg.style.filter = `contrast(${scale(load, 0, 200, 30, 0)}px)`
+    bg.style = `grayscale(${scale(load, 0, 50, 30, 0)}px)`
+    bg.style.filter = `opacity(${scale(load, 0, 40, 30, 0)}px)`
+}
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+  }
+
+
 jQuery(window).on('load', function() {
         
     // HIDE PRELAODER
